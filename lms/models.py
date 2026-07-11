4c3bfa87d9605f93f6648dcd27ff41b5e8871c02
Olesia Laskovets <olesya_izvarina@mail.ru>
2026-07-11 15:29:46 +0300

    Implement JWT authentication and permissions

--- lms/models.py
+++ lms/models.py
@@ -2,25 +2,61 @@ from django.db import models
 
 
 class Course(models.Model):
+    owner = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="courses",
+        null=True,
+        blank=True,
+    )
+
     title = models.CharField(max_length=255)
-    preview = models.ImageField(upload_to="course_previews/", blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    preview = models.ImageField(
+        upload_to="course_previews/",
+        blank=True,
+        null=True
+    )
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.title
 
 
 class Lesson(models.Model):
+    owner = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="lessons",
+        null=True,
+        blank=True,
+    )
+
     course = models.ForeignKey(
-        "Course",  # 👈 ВОТ ГЛАВНОЕ ИСПРАВЛЕНИЕ
+        Course,
         on_delete=models.CASCADE,
         related_name="lessons"
     )
 
     title = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    preview = models.ImageField(upload_to="lesson_previews/", blank=True, null=True)
-    video_url = models.URLField(blank=True, null=True)
+
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    preview = models.ImageField(
+        upload_to="lesson_previews/",
+        blank=True,
+        null=True
+    )
+
+    video_url = models.URLField(
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.title
--- lms/views.py
+++ lms/views.py
@@ -1,26 +1,102 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import Course
-from .serializers import CourseSerializer
-from .models import Lesson
-from .serializers import LessonSerializer
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
+from .permissions import IsModerator, IsOwner
+
 
 class CourseViewSet(ModelViewSet):
-    queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def get_queryset(self):
+        user = self.request.user
 
-from rest_framework.generics import (
-    ListCreateAPIView,
-    RetrieveUpdateDestroyAPIView
-)
+        if user.groups.filter(name="moderators").exists():
+            return Course.objects.all()
+
+        return Course.objects.filter(owner=user)
+
+    def get_permissions(self):
+
+        if self.action in ["update", "partial_update"]:
+            permission_classes = [
+                IsAuthenticated,
+                IsOwner | IsModerator
+            ]
+
+        elif self.action == "destroy":
+            permission_classes = [
+                IsAuthenticated,
+                IsOwner
+            ]
+
+        elif self.action == "create":
+            permission_classes = [
+                IsAuthenticated,
+                ~IsModerator
+            ]
+
+        else:
+            permission_classes = [
+                IsAuthenticated
+            ]
+
+        return [
+            permission()
+            for permission in permission_classes
+        ]
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 
-class LessonListCreateAPIView(ListCreateAPIView):
-    queryset = Lesson.objects.all()
+class LessonViewSet(ModelViewSet):
     serializer_class = LessonSerializer
 
 
-class LessonDetailAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Lesson.objects.all()
-    serializer_class = LessonSerializer
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.groups.filter(name="moderators").exists():
+            return Lesson.objects.all()
+
+        return Lesson.objects.filter(owner=user)
+
+
+
+    def get_permissions(self):
+
+        if self.action in ["update", "partial_update"]:
+            permission_classes = [
+                IsAuthenticated,
+                IsOwner | IsModerator
+            ]
+
+        elif self.action == "destroy":
+            permission_classes = [
+                IsAuthenticated,
+                IsOwner
+            ]
+
+        elif self.action == "create":
+            permission_classes = [
+                IsAuthenticated,
+                ~IsModerator
+            ]
+
+        else:
+            permission_classes = [
+                IsAuthenticated
+            ]
+
+        return [
+            permission()
+            for permission in permission_classes
+        ]
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
